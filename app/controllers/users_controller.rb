1b4cf5bcf199348848e938bd05744664c4cf48e4
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-01 19:23:46 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
-  def create 
+  def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id 
-      redirect_to :welcome 
-    else 
-      redirect_to :new
-    end 
+      session[:user_id] = @user.id
+      redirect_to homepage_path(@user)
+    else
+      redirect_to '/'
+    end
   end
   
   private 
