5b477cc02e1b7df2f3d9df1bb11c5c638556e7e7
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-01 19:18:38 +0000

    Automatically backed up by Learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController 
+  def new 
+    @user = User.new 
+  end 
+  
+  def create 
+    @user = User.find_by(name: params[:password])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id 
+      redirect_to :welcome
+    else
+      flash[:notice] = "Login is not correct"
+      redirect_to :login 
+    end 
+  end
+end
