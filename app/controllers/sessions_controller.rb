b877402b7ed67242cdc6432367c47f3f65ae3039
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-01 19:22:44 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(name: params[:user][:name])
     if @user == nil
-      redirect_to 'new'
+      redirect_to :welcome
     else
       return head(:forbidden) unless @user.authenticate(params[:user][:password])
       session[:user_id] = @user.id
