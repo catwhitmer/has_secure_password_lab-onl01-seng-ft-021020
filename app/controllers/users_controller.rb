ef06c508d2c90149aa2649a10741706dddb1ce60
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-01 19:31:16 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = @user.id
       redirect_to :welcome
     else
-      redirect_to 'users/new'
+      redirect_to :new
     end
   end
   
