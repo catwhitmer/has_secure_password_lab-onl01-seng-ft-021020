2c3df03cabaa8777e33645431afb85a2b571923a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-01 19:27:34 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,8 @@ Rails.application.routes.draw do
   get "/welcome", to: "welcome#new"
 end
 
+  get '/', to: 'users#new', as: 'user'
+  post '/', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/:user', to: 'users#show', as: 'homepage'
